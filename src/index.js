c5483b1ee85ef501a20dda94aef70c7179cd4523
Sage <prestoferoce@gmail.com>
2024-01-11 22:32:51 +0900

    Feat: Footer 컴포넌트 HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import App from "./components/App.js";
 import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer.js";
 
 customElements.define("blog-app", App);
 customElements.define("blog-header", Header);
+customElements.define("blog-footer", Footer);
