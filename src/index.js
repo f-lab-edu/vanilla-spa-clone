5ae1b1ec564482b0462dbce74821d9d1dfa9988a
Sage <prestoferoce@gmail.com>
2024-01-20 23:21:27 +0900

    Feat: FooterIconList 컴포넌트 분리

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import App from "./components/App.js";
 import Header from "./components/Header/Header.js";
 import Footer from "./components/Footer/Footer.js";
 import FooterList from "./components/FooterList/FooterList.js";
+import FooterIconList from "./components/FooterIconList/FooterIconList.js";
 import ArticleList from "./components/ArticleList/ArticleList.js";
 import ArticleItem from "./components/ArticleItem/ArticleItem.js";
 
@@ -9,5 +10,6 @@ customElements.define("blog-app", App);
 customElements.define("blog-header", Header);
 customElements.define("blog-footer", Footer);
 customElements.define("footer-list", FooterList);
+customElements.define("footer-icon-list", FooterIconList);
 customElements.define("article-list", ArticleList);
 customElements.define("article-item", ArticleItem);
