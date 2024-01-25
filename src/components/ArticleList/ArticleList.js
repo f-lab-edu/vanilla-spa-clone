7ea83cb979dbf3bed6365ed67a32566b08a19394
Sage <prestoferoce@gmail.com>
2024-01-25 23:21:01 +0900

    Feat: ArticleList 구현

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,12 +1,49 @@
-import "./style.css";
+import template from "./template.html";
+import style from "./style.css?shadow";
+
+// 임시 정적 데이터
+import data from "../../data/mockArticle.json";
+
+// 임시 현재 경로
+const CURRENT_PATH = "tech";
+
+const TITLE_MAP = {
+  tech: "개발",
+  design: "디자인",
+};
 
 export default class ArticleList extends HTMLElement {
   constructor() {
     super();
-    this.template = document.getElementById("article-list");
+
+    if (!this.shadowRoot) {
+      this.attachShadow({ mode: "open" });
+    }
+  }
+
+  addArticles(ulElement, articles) {
+    articles.forEach((item) => {
+      const articleItemElement = document.createElement("article-item");
+      articleItemElement.setData(item);
+      ulElement.appendChild(articleItemElement);
+    });
   }
 
   connectedCallback() {
-    this.appendChild(this.template.content.cloneNode(true));
+    const HTMLTemplate = new DOMParser()
+      .parseFromString(template, "text/html")
+      .querySelector("template").content;
+    const styleElement = document.createElement("style");
+    styleElement.textContent = style;
+
+    const titleElement = HTMLTemplate.querySelector(".article-list__title");
+    titleElement.textContent = TITLE_MAP[CURRENT_PATH];
+
+    const ulElement = HTMLTemplate.querySelector(".article-list__ul");
+
+    this.addArticles(ulElement, data.results);
+
+    this.shadowRoot.appendChild(HTMLTemplate.cloneNode(true));
+    this.shadowRoot.appendChild(styleElement);
   }
 }
