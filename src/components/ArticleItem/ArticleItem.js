28d33f4165cbf2641befde3fd24d9dd426e8153c
Sage <prestoferoce@gmail.com>
2024-01-25 23:21:01 +0900

    Feat: ArticleList 구현

--- src/components/ArticleItem/ArticleItem.js
+++ src/components/ArticleItem/ArticleItem.js
@@ -4,8 +4,12 @@ import style from "./style.css?shadow";
 export default class ArticleItem extends HTMLElement {
   constructor() {
     super();
+
+    if (!this.shadowRoot) {
+      this.attachShadow({ mode: "open" });
+    }
+
     this.data = {};
-    this.attachShadow({ mode: "open" });
   }
 
   setData(data) {
@@ -25,23 +29,21 @@ export default class ArticleItem extends HTMLElement {
   render() {
     const HTMLTemplate = new DOMParser()
       .parseFromString(template, "text/html")
-      .querySelector("template");
+      .querySelector("template").content;
     const styleElement = document.createElement("style");
     styleElement.textContent = style;
 
-    HTMLTemplate.content.querySelector(".article-item__a").href =
+    HTMLTemplate.querySelector(".article-item__a").href =
       "article/" + this.data.id;
-    HTMLTemplate.content.querySelector(".article-item__image").src =
-      this.data.imageUrl;
-    HTMLTemplate.content.querySelector(".article-item__title").textContent =
+    HTMLTemplate.querySelector(".article-item__image").src = this.data.imageUrl;
+    HTMLTemplate.querySelector(".article-item__title").textContent =
       this.data.title;
-    HTMLTemplate.content.querySelector(
-      ".article-item__description"
-    ).textContent = this.data.description;
-    HTMLTemplate.content.querySelector(".article-item__date").textContent =
+    HTMLTemplate.querySelector(".article-item__description").textContent =
+      this.data.description;
+    HTMLTemplate.querySelector(".article-item__date").textContent =
       this.formatDate(this.data.createdAt);
 
-    this.shadowRoot.appendChild(HTMLTemplate.content.cloneNode(true));
+    this.shadowRoot.appendChild(HTMLTemplate.cloneNode(true));
     this.shadowRoot.appendChild(styleElement);
   }
 }
