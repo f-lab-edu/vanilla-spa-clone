1d19dc639cd40c43d7032842709d9b5314bc57e6
Sage <prestoferoce@gmail.com>
2024-01-25 22:21:31 +0900

    Feat: ArticleItem 컴포넌트 템플릿 분리, setData 및 render 메소드 추가
    - template HTML 분리
    - Shadow DOM 적용
    - CSS 추가
    - ArticleList에서 받은 Data 설정하는 setData 메소드 추가
    - data로 렌더링 하는 render 메소드 추가

--- src/components/ArticleItem/ArticleItem.js
+++ src/components/ArticleItem/ArticleItem.js
@@ -1,12 +1,37 @@
-import "./style.css";
+import template from "./template.html";
+import style from "./style.css?shadow";
 
 export default class ArticleItem extends HTMLElement {
   constructor() {
     super();
-    this.template = document.getElementById("article-item");
+    this.data = {};
+
+    const HTMLTemplate = new DOMParser()
+      .parseFromString(template, "text/html")
+      .querySelector("template");
+    const styleElement = document.createElement("style");
+    styleElement.textContent = style;
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(HTMLTemplate.content.cloneNode(true));
+    this.shadowRoot.appendChild(styleElement);
   }
 
-  connectedCallback() {
-    this.appendChild(this.template.content.cloneNode(true));
+  setData(data) {
+    this.data = data;
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.querySelector(".article-item__a").href =
+      "article/" + this.data.id;
+    this.shadowRoot.querySelector(".article-item__image").src =
+      this.data.imageUrl;
+    this.shadowRoot.querySelector(".article-item__title").textContent =
+      this.data.title;
+    this.shadowRoot.querySelector(".article-item__description").textContent =
+      this.data.description;
+    this.shadowRoot.querySelector(".article-item__date").textContent =
+      this.data.createdAt;
   }
 }
