9b349a32abd9983eca8d3cd404e7f132759ec8aa
Sage <prestoferoce@gmail.com>
2024-02-01 07:26:23 +0900

    Chore: CopyWebpackPlugin 설치 및 웹팩 설정 수정
    - 불필요한 alias 설정 제거
    - 웹팩 production에 CopyWebpackPlugin 설정 추가

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "",
+          globOptions: { ignore: ["**/index.html", "**/assets"] },
+        },
+      ],
+    }),
+  ],
 });
