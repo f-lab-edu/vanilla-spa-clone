c77a16571ce1b436abd92998dbd1827ee01bcb26
Sage <prestoferoce@gmail.com>
2024-02-01 02:25:42 +0900

    Update: ArticleItem Shadow DOM 삭제 및 formatDate 분리

---
+++ src/utils/formatDate.js
@@ -0,0 +1,8 @@
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}. ${month}. ${day}`;
+};
