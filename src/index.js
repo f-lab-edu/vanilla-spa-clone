3bdc9a79032046b59112c8af4baf909f497ac431
Sage <prestoferoce@gmail.com>
2024-02-03 06:50:08 +0900

    Feat: Router 구현

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import FooterList from "./components/FooterList/FooterList.js";
 import FooterIconList from "./components/FooterIconList/FooterIconList.js";
 import ArticleList from "./components/ArticleList/ArticleList.js";
 import ArticleItem from "./components/ArticleItem/ArticleItem.js";
+import Router from "./router/router.js";
 
 customElements.define("blog-app", App);
 customElements.define("blog-header", Header);
@@ -13,3 +14,10 @@ customElements.define("footer-list", FooterList);
 customElements.define("footer-icon-list", FooterIconList);
 customElements.define("article-list", ArticleList);
 customElements.define("article-item", ArticleItem);
+
+const router = new Router();
+router.addRoute("/", "tech");
+router.addRoute("/tech", "tech");
+router.addRoute("/design", "design");
+router.addRoute("/article/:id", "article");
+router.start();
