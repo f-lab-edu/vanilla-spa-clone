d191ed30eb5b906a65f2664a26a339a16ba58120
Sage <prestoferoce@gmail.com>
2024-02-03 23:24:18 +0900

    Feat: 각 Page 생성 및 router 연결

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import FooterIconList from "./components/FooterIconList/FooterIconList.js";
 import ArticleList from "./components/ArticleList/ArticleList.js";
 import ArticleItem from "./components/ArticleItem/ArticleItem.js";
 import Router from "./router/router.js";
+import ArticleListPage from "./pages/ArticleListPage.js";
+import ArticlePage from "./pages/ArticlePage.js";
+import NotFoundPage from "./pages/NotFoundPage.js";
 
 customElements.define("blog-app", App);
 customElements.define("blog-header", Header);
@@ -16,8 +19,10 @@ customElements.define("article-list", ArticleList);
 customElements.define("article-item", ArticleItem);
 
 const router = new Router();
-router.addRoute("/", "tech");
-router.addRoute("/tech", "tech");
-router.addRoute("/design", "design");
-router.addRoute("/article/:id", "article");
+
+router.addRoute("/", () => ArticleListPage("tech"));
+router.addRoute("/tech", () => ArticleListPage("tech"));
+router.addRoute("/design", () => ArticleListPage("design"));
+router.addRoute("/article/:id", ArticlePage);
+router.setNotFound(NotFoundPage);
 router.start();
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,3 @@
+const ArticlePage = (params) => {};
+
+export default ArticlePage;
