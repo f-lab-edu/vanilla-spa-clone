1e8b5326e7bbbe16388ca40c715960319b4a4124
Sage <prestoferoce@gmail.com>
2024-02-04 07:27:10 +0900

    Update: 임시 아티클 페이지

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,3 +1,8 @@
-const ArticlePage = (params) => {};
+const ArticlePage = (params) => {
+  const div = document.createElement("div");
+  div.textContent = `아티클 ID: ${params.id}`;
+
+  return div;
+};
 
 export default ArticlePage;
