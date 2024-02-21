2a2a5563380510d8c8ea23a0f4eda09b2abea9ba
Sage <prestoferoce@gmail.com>
2024-02-21 20:08:10 +0900

    Feat: Footer 타입스크립트로 변경

--- src/components/Footer/Footer.ts
+++ src/components/Footer/Footer.ts
@@ -2,14 +2,18 @@ import template from "./template.html";
 import "./style.css";
 
 export default class Footer extends HTMLElement {
+  private template: DocumentFragment | undefined;
+
   constructor() {
     super();
     this.template = new DOMParser()
       .parseFromString(template, "text/html")
-      .querySelector("template").content;
+      .querySelector("template")?.content;
   }
 
-  connectedCallback() {
-    this.appendChild(this.template.cloneNode(true));
+  connectedCallback(): void {
+    if (this.template) {
+      this.appendChild(this.template.cloneNode(true));
+    }
   }
 }
