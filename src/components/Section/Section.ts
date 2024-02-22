8998ca7660bb7e3c989ae26680604689645ec19e
Sage <prestoferoce@gmail.com>
2024-02-22 18:59:59 +0900

    Feat: Section TypeScript 적용

---
+++ src/components/Section/Section.ts
@@ -0,0 +1,26 @@
+import Router from "@/router/router";
+import ArticleListPage from "@/pages/ArticleListPage";
+import ArticlePage from "@/pages/ArticlePage";
+import NotFoundPage from "@/pages/NotFoundPage";
+
+export default class Section extends HTMLElement {
+  private router: Router;
+
+  constructor() {
+    super();
+    this.router = new Router(this);
+    this.configureRouting();
+  }
+
+  configureRouting(): void {
+    this.router.addRoute("/", () => ArticleListPage("tech"));
+    this.router.addRoute("/tech", () => ArticleListPage("tech"));
+    this.router.addRoute("/design", () => ArticleListPage("design"));
+    this.router.addRoute("/article/:id", (params) => ArticlePage(params));
+    this.router.setNotFoundPage(() => NotFoundPage());
+  }
+
+  connectedCallback(): void {
+    this.router.start();
+  }
+}
