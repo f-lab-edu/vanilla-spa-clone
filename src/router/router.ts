505a59aca27b1f4b755dc8c45e8b12a97a5aaa77
Sage <prestoferoce@gmail.com>
2024-02-22 20:26:17 +0900

    Feat: Router TypeScript 적용 및 pageNavigation 이벤트 수정

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,67 +1,88 @@
+import Section from "@/components/Section/Section";
+
+interface Routes {
+  regex: RegExp;
+  page: any;
+  params: string[];
+}
+
 export default class Router {
-  constructor(section) {
-    this._section = section;
-    this._routes = [];
-    this._currentPath = null;
-    this._notFoundPage = null;
+  private section: Section;
+  private routes: Routes[] = [];
+  private currentPath: string | null = null;
+  private notFoundPage: any = null;
+
+  constructor(section: Section) {
+    this.section = section;
   }
 
-  addRoute(path, page) {
-    const params = [];
+  addRoute(path: string, page: any): void {
+    const params: string[] = [];
 
-    const parsedPath = path.replace(/:(\w+)/g, (_, paramName) => {
+    const parsedPath = path.replace(/:(\w+)/g, (substring, paramName) => {
       params.push(paramName);
 
       return "([^/]+)";
     });
 
     const regex = new RegExp(`^${parsedPath}$`);
-    this._routes.push({ regex, page, params });
+    this.routes.push({ regex, page, params });
   }
 
-  extractParams(route, pathname) {
+  extractParams(route: Routes, pathname: string): Record<string, string> {
     const match = route.regex.exec(pathname);
 
-    const params = match.slice(1).reduce((params, value, index) => {
-      params[route.params[index]] = value;
-      return params;
-    }, {});
+    const params =
+      match?.slice(1).reduce((acc: Record<string, string>, value, index) => {
+        acc[route.params[index]] = value;
+        return acc;
+      }, {}) ?? {};
 
     return params;
   }
 
-  setNotFoundPage(page) {
-    this._notFoundPage = page;
+  setNotFoundPage(page: any): void {
+    this.notFoundPage = page;
   }
 
-  navigate(path) {
-    history.pushState(null, null, path);
+  navigate(path: string): void {
+    history.pushState("", "", path);
     this.checkRoutes();
   }
 
-  checkRoutes() {
+  checkRoutes(): void {
     const { pathname } = location;
 
-    if (this._currentPath === pathname) return;
+    if (this.currentPath === pathname) return;
 
-    this._currentPath = pathname;
+    this.currentPath = pathname;
 
-    const route = this._routes.find(({ regex }) => regex.test(pathname));
+    const route = this.routes.find(({ regex }) => regex.test(pathname));
 
-    this._section.innerHTML = "";
+    this.section.innerHTML = "";
 
     if (!route) {
-      this._section.appendChild(this._notFoundPage());
+      this.section.appendChild(this.notFoundPage());
       return;
     }
 
     const params = this.extractParams(route, pathname);
 
-    this._section.appendChild(route.page(params));
+    this.section.appendChild(route.page(params));
   }
 
-  start() {
+  start(): void {
     window.addEventListener("popstate", () => this.checkRoutes());
+    window.addEventListener("pageNavigation", (event) =>
+      this.navigate(event.detail.path)
+    );
     this.checkRoutes();
+  }
+
+  stop(): void {
+    window.removeEventListener("popstate", () => this.checkRoutes());
+    window.removeEventListener("pageNavigation", (event) =>
+      this.navigate(event.detail.path)
+    );
   }
 }
