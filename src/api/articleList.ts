20aefe1440dd9e171172e9fb7a4b02eb02c0df53
Sage <prestoferoce@gmail.com>
2024-02-22 12:08:05 +0900

    Feat: axios articleList TypeScript 적용
    - articleList 타입스크립트 적용
    - types.d.ts에 Article 타입 생성

---
+++ src/api/articleList.ts
@@ -0,0 +1,41 @@
+import request from "./axios";
+import axios, { AxiosResponse } from "axios";
+import { Article } from "@/types/types";
+
+export const getTechArticleList = async (): Promise<Article[]> => {
+  try {
+    const response: AxiosResponse = await request({
+      method: "GET",
+      url: "data/mockTechArticleList.json",
+    });
+
+    const data: Article[] = response.data.results;
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError<{ message: string }>(error)) {
+      console.log(error.response?.data.message);
+    }
+
+    throw error;
+  }
+};
+
+export const getDesignArticleList = async (): Promise<Article[]> => {
+  try {
+    const response: AxiosResponse = await request({
+      method: "GET",
+      url: "data/mockDesignArticleList.json",
+    });
+
+    const data: Article[] = await response.data.results;
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError<{ message: string }>(error)) {
+      console.log(error.response?.data.message);
+    }
+
+    throw error;
+  }
+};
