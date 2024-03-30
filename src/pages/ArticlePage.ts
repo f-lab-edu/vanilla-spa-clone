6d2a548ba4e90321e263f8acc79754d5c04c1890
Sage <prestoferoce@gmail.com>
2024-03-30 19:59:45 +0900

    Fix: Page 컴포넌트 타입 추론 사용

--- src/pages/ArticlePage.ts
+++ src/pages/ArticlePage.ts
@@ -1,4 +1,4 @@
-const ArticlePage = (params: Record<string, string>): HTMLDivElement => {
+const ArticlePage = (params: Record<string, string>) => {
   const div: HTMLDivElement = document.createElement("div");
   div.textContent = `아티클 ID: ${params.id}`;
 
