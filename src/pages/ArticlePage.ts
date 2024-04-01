e71ac2ee0d67eb6f25025094389d56e6e2b67d7e
Sage <prestoferoce@gmail.com>
2024-04-02 02:36:09 +0900

    Fix: ArticlePage에서 params 없을 때 및 Not Found 페이지 없을 때 처리

--- src/pages/ArticlePage.ts
+++ src/pages/ArticlePage.ts
@@ -1,4 +1,10 @@
-const ArticlePage = (params: Record<string, string>) => {
+import NotFoundPage from "./NotFoundPage";
+
+const ArticlePage = (params?: Record<string, string>) => {
+  if (!params) {
+    return NotFoundPage();
+  }
+
   const div: HTMLDivElement = document.createElement("div");
   div.textContent = `아티클 ID: ${params.id}`;
 
